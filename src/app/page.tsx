a0a9bcd76646dfd82db28dcdfcaff55e0ff3e863
Spenser Hale <opensource@spenserhale.dev>
2024-05-03 20:05:09 -0700

    + image schema & connected to ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,11 @@
 import Link from "next/link";
+import {db} from "~/server/db";
 
-const urls = [
-  "https://w.wallhaven.cc/full/m3/wallhaven-m3m57k.jpg",
-  "https://w.wallhaven.cc/full/7p/wallhaven-7pqvzv.jpg",
-  "https://w.wallhaven.cc/full/5g/wallhaven-5g8r55.jpg",
-  "https://w.wallhaven.cc/full/jx/wallhaven-jxqrw5.jpg",
-  "https://w.wallhaven.cc/full/p9/wallhaven-p9x1ee.jpg",
-  "https://w.wallhaven.cc/full/gp/wallhaven-gpxl97.jpg",
-];
+export default async function HomePage() {
+  const images = await db.query.images.findMany({
+    orderBy: (model, {desc}) => desc(model.id)
+  });
 
-const images = urls.map((url, index) => ({
-  id: index + 1,
-  url,
-}));
-
-export default function HomePage() {
   return (
     <main className="">
       <div className="container flex flex-wrap gap-4">{
